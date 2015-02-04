a79cd24068d5b9997c70f2fdc158d28893842276
Pichet <pichet.nk@gmail.com>
2015-02-04 23:24:08 +0700

    update config file

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 	$openBookingRound=array(1,2,3,4,5,6,7,8);
 	$db_host="127.0.0.1";
-	$db_name="nourneer";
-	$db_user="webadmin";
-	$db_pw="Techno48";
+	$db_name="****";
+	$db_user="***";
+	$db_pw="***";
 	try {
 		$conn = new PDO('mysql:host='.$db_host.'; dbname='.$db_name, $db_user, $db_pw);
  		$conn->exec("SET CHARACTER SET utf8");
